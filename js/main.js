7bc4b7a76003aae0eba031fc87ae55015dd028db
sts2606 <staspetrenko2606@gmail.com>
2020-01-14 22:54:19 +0200

    add burger

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
 console.log('work');
 
-// let menuButton = document
+let menuButton = document.getElementById('menuButton');
+let menuBlock = document.getElementById('menuBlock');
+let signButton = document.getElementById('signButton');
+let signBlock = document.getElementById('signBlock');
+
+menuButton.addEventListener('click' , () => {
+    menuBlock.classList.toggle('hide')
+    menuButton.classList.toggle('fa-angle-double-up')
+    menuButton.classList.toggle('fa-bars')
+    signBlock.classList.remove('hide')
+    signBlock.classList.add('hide')
+    signButton.classList.remove('fa-angle-double-up')
+    signButton.classList.add('fa-user')
+})
+
+signButton.addEventListener('click' , () => {
+    signBlock.classList.toggle('hide')
+    signButton.classList.toggle('fa-angle-double-up')
+    signButton.classList.toggle('fa-user')
+    menuBlock.classList.remove('hide')
+    menuBlock.classList.add('hide')
+    menuButton.classList.remove('fa-angle-double-up')
+    menuButton.classList.add('fa-bars')
+
+})
